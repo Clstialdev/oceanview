6a783a9a2e76c21d035eb53a644b3b897285cc5f
clstialdsn <clstialdsn@Clstials-MacBook-Pro.local>
2023-02-01 14:04:58 +0100

    Header Base

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { type NextPage } from "next";
+
+const Header: NextPage = () => {
+  return (
+    <header className="flex h-[100px] w-full items-center justify-between bg-opacity-20 bg-gradient-to-b from-black to-white px-[7%] [&>*]:text-white">
+      {/* Left Side (branding) */}
+      <div className="flex items-center justify-center gap-2">
+        {/* Logo */}
+        <div>
+          <h1>PLHLDR</h1>
+        </div>
+        {/* Typography */}
+        <div>
+          <h1 className="text-center">Ocean View</h1>
+          <h2 className="text-center">Games</h2>
+        </div>
+      </div>
+      {/* Right Side (navigation) */}
+      <nav className="flex gap-12">
+        <Link href="#projects">Projects</Link>
+        <Link href="#services">Services</Link>
+        <Link href="#team">Team</Link>
+        <Link href="#contact">Contact</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
