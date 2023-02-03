fe74908ff4ed1b12390fb5002bf42fff8a6ab31d
Clstialdev <Rusty75001@gmail.com>
2023-02-03 03:12:11 +0100

    feature:Service Card Animation Clean

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,7 +1,13 @@
 import { type NextPage } from "next";
 import Image from "next/image";
-import { motion, useMotionValue } from "framer-motion";
-import { useEffect, useRef, useState } from "react";
+import {
+  animate,
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+  useTransform,
+} from "framer-motion";
+import { useEffect, useRef, useState, useTransition } from "react";
 
 interface ServiceCardProps {
   image: string;
@@ -17,56 +23,77 @@ const ServiceCard: NextPage<ServiceCardProps> = ({
 }) => {
   const cardRef = useRef<HTMLDivElement>(null);
 
-  const [offsetX, setOffsetX] = useState(0);
-  const [offsetY, setOffsetY] = useState(0);
-
-  const getMouseLocation = (e: MouseEvent) => {
-    const cardElement = cardRef.current as HTMLDivElement;
-
-    const X = e.pageX - cardElement.offsetLeft - cardElement.offsetWidth / 2;
-    const Y = e.screenY - cardElement.offsetTop - cardElement.offsetHeight / 2;
-
-    const ySign = Y >= 0 ? 1 : -1;
-    const xSign = X >= 0 ? 1 : -1;
-
-    const correctedX = -X * ySign;
-    const correctedY = Y * xSign;
-
-    setOffsetX(correctedX);
-    setOffsetY(correctedY);
-    console.log("X=", correctedX);
-    console.log("Y=", correctedY);
-  };
-
-  const animateCard = (e: MouseEvent) => {
-    const cardElement = cardRef.current as HTMLDivElement;
-    const x = e.pageX - (cardElement.offsetLeft ?? 0);
-    const y = e.clientY - (cardElement.offsetTop ?? 0);
-  };
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+  const scaleV = useMotionValue(10);
 
   useEffect(() => {
     const cardElement = cardRef.current as HTMLDivElement;
-    cardElement.addEventListener("mousemove", (e) => getMouseLocation(e));
-    cardElement.addEventListener("mouseout", () => {
-      setOffsetX(0);
-      setOffsetY(0);
-    });
+    const handleMouseMove = (e: MouseEvent) => {
+      const X = e.pageX - cardElement.offsetLeft - cardElement.offsetWidth / 2;
+      const Y =
+        e.screenY - cardElement.offsetTop - cardElement.offsetHeight / 2;
 
+      const ySign = Y >= 0 ? 1 : -1;
+      const xSign = X >= 0 ? 1 : -1;
+
+      // const correctedX = X * ySign;
+      // const correctedY = Y * xSign;
+
+      animate(mouseX, X);
+      animate(mouseY, Y);
+      animate(scaleV, 2);
+    };
+
+    const handleReset = () => {
+      animate(mouseX, 0);
+      animate(mouseY, 0);
+      animate(scaleV, 1);
+    };
+
+    if (typeof window === "undefined") return;
+    cardElement.addEventListener("mousemove", handleMouseMove);
+    cardElement.addEventListener("mouseleave", handleReset);
     return () => {
-      cardElement.removeEventListener("mousemove", (e) => getMouseLocation(e));
-      cardElement.removeEventListener("mouseout", () => {
-        setOffsetX(0);
-        setOffsetY(0);
-      });
+      cardElement.removeEventListener("mousemove", handleMouseMove);
+      cardElement.removeEventListener("mouseleave", handleReset);
     };
   }, []);
 
+  const rotateX = useTransform(mouseX, [300, -300], [-10, 10]);
+  const rotateY = useTransform(mouseY, [-300, 300], [-5, 10]);
+  const scale = useTransform(scaleV, [1, 2], [1, 1.03]);
+
+  const diagonalMovement = useTransform<number, number>(
+    [rotateX, rotateY],
+    ([newRotateX, newRotateY]) => {
+      const position: number = newRotateX + newRotateY;
+      return position;
+    }
+  );
+  const sheenPosition = useTransform(diagonalMovement, [-5, 5], [-100, 200]);
+
+  const sheenOpacity = useTransform(
+    sheenPosition,
+    [-100, 100, -400],
+    [0, 0.15, 0]
+  );
+  const sheenGradient = useMotionTemplate`linear-gradient(
+    55deg,
+    transparent,
+    rgba(81 130 118 / ${sheenOpacity}) ${sheenPosition}%,
+    transparent)`;
+
   return (
     <motion.div
-      whileHover={{ rotateZ: offsetX * 0.015, skewY: offsetY * 0.01 }}
+      style={{ rotateX, rotateY, scale }}
       className="relative h-[400px] w-[40vw] max-w-[580px] rounded-lg bg-services-light p-8"
       ref={cardRef}
     >
+      <motion.div
+        className="absolute left-0 top-0 right-0 bottom-0"
+        style={{ backgroundImage: sheenGradient }}
+      ></motion.div>
       {/* Image */}
       <div className="relative h-[65%] w-full">
         <Image
