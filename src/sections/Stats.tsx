ae746549fdceb2b8415b69b2b2b9584dbd9a3ba1
Clstialdev <Rusty75001@gmail.com>
2023-02-03 04:04:02 +0100

    Stats section

--- src/sections/Stats.tsx
+++ src/sections/Stats.tsx
@@ -1,4 +1,5 @@
 import { type NextPage } from "next";
+import { useEffect, useRef, useState } from "react";
 import SectionHeading from "../components/SectionHeading";
 
 const STATS = [
@@ -8,17 +9,46 @@ const STATS = [
 ];
 
 const Stats: NextPage = () => {
+  const [value, setValue] = useState(0);
+
+  const statsRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const counter = () => {
+      if (value < 100) {
+        setTimeout(() => {
+          setValue(value + 1);
+        }, 2);
+
+        console.log("counter");
+      }
+    };
+
+    if (value >= 1) {
+      counter();
+    }
+
+    statsRef.current?.addEventListener("mouseenter", () => counter());
+
+    return statsRef.current?.removeEventListener("mouseenter", () => counter());
+  }, [value]);
+
   return (
-    <div className="w-full py-12 px-[10%]">
+    <div
+      className="w-full bg-gradient-to-b from-[#040D10] to-[#07041A] py-12 px-[10%]"
+      ref={statsRef}
+    >
       {/* Section Heading */}
       <SectionHeading title="Some Numbers" />
 
       {/* Stats */}
-      <div className="flex w-full items-center justify-around">
+      <div className="my-20 flex w-full items-center justify-between">
         {STATS.map((stat, index) => (
-          <div key={index}>
-            <h1 className="text-8xl text-white">{stat.value}</h1>
-            <h2 className="text-6xl uppercase">{stat.name}</h2>
+          <div key={index} className="flex flex-col items-center">
+            <h1 className="font-alatsi mb-2 text-8xl text-white">{value}</h1>
+            <h2 className="font-alatsi text-2xl uppercase text-[#5A88BD] lg:text-4xl">
+              {stat.name}
+            </h2>
           </div>
         ))}
       </div>
