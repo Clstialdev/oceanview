c5cb3bf87dd7079694e908eac6dda492fb2bffce
Clstialdev <Rusty75001@gmail.com>
2023-03-05 17:29:55 +0100

    added security headers

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,18 @@
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
 
 /** @type {import("next").NextConfig} */
+
+const securityHeaders = [{ key: "X-DNS-Prefetch-Control", value: "on" }];
 const config = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
   reactStrictMode: true,
   /* If trying out the experimental appDir, comment the i18n config out
    * @see https://github.com/vercel/next.js/issues/41980 */
